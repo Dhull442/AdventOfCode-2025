6d8eadb62c9f3709f3eb98b6c5e04ec3c74b8ec1
Saksham <sakshamdhull442@gmail.com>
2025-12-03 20:11:49 +0530

    day-3-part-2

--- day-3/main.cpp
+++ day-3/main.cpp
@@ -2,27 +2,29 @@
 #include<fstream>
 using namespace std;
 #define lli long long int
-int maxJoltage(string line){
-    string out = line.substr(line.size()-2);
-    for(int i=line.size()-3;i>=0;i--){
-        if(line[i]>=out[0]){
-            if(out[0]>=out[1]){
-                out[1] = out[0];
-                out[0] = line[i];
+lli maxJoltage(string line, int len){
+    string out = line.substr(line.size()-len);
+    for(int i=line.size()-(len+1);i>=0;i--){
+        char carry = line[i];
+        for(int j=0;j<out.size();j++){
+            if(out[j]<=carry){
+                char newcarry = out[j];
+                out[j] = carry;
+                carry = newcarry;
             } else {
-                out[0] = line[i];
+                break;
             }
         }
     }
     cout << out << ", ";
-    return stoi(out);
+    return stoll(out);
 }
 int main(){
     lli count = 0;
     ifstream file("input.txt");
     string line;
     while(file >> line){
-        count += maxJoltage(line);
+        count += maxJoltage(line, 12);
     }
     cout << "MAX JOLTAGE - " << count << endl;
 }
